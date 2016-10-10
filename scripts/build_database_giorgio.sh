66b01484416d6bbe36b3ef0fbd2d27968ead3c71
Manuel Bonilla <josemanuelbonilla@gmail.com>
2016-10-10 14:49:59 +0900

    Adding script for giorgio pc

---
+++ scripts/build_database_giorgio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+####################################
+python test_mvbb_filtered.py moutain_security_steel_shackle 
+python test_mvbb_filtered.py moutain_security_steel_shackle_key 
+python test_mvbb_filtered.py orange_wood_block_1inx1in 
+python test_mvbb_filtered.py penn_raquet_ball 
+python test_mvbb_filtered.py plastic_bolt_grey 
+python test_mvbb_filtered.py plastic_nut_grey 
+python test_mvbb_filtered.py plastic_wine_cup
