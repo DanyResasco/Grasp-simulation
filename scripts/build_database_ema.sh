04d9d774f35bc1a31387a3de310749352a1e1e7c
Manuel Bonilla <josemanuelbonilla@gmail.com>
2016-10-10 14:38:57 +0900

    scripting

---
+++ scripts/build_database_ema.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+####################################
+python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_lemon
+python test_mvbb_filtered.py champion_sports_official_softball
+python test_mvbb_filtered.py penn_raquet_ball
+python test_mvbb_filtered.py wilson_100_tennis_ball
+python test_mvbb_filtered.py rubbermaid_ice_guard_pitcher_blue
+python test_mvbb_filtered.py clorox_disinfecting_wipes_35
+python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_orange
+python test_mvbb_filtered.py champion_copper_plus_spark_plug
+python test_mvbb_filtered.py cheezit_big_original
+python test_mvbb_filtered.py crayola_64_ct
+python test_mvbb_filtered.py feline_greenies_dental_treats
+python test_mvbb_filtered.py highland_6539_self_stick_notes
+python test_mvbb_filtered.py laugh_out_loud_joke_book
+python test_mvbb_filtered.py one_with_nature_soap_dead_sea_mud
+python test_mvbb_filtered.py black_and_decker_lithium_drill_driver_unboxed
---
+++ scripts/build_database_mx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+####################################
+python test_mvbb_filtered.py red_metal_bowl_white_speckles
+python test_mvbb_filtered.py blank_hard_plastic_card 
+python test_mvbb_filtered.py stainless_steel_knife_red_handle 
+python test_mvbb_filtered.py elmers_washable_no_run_school_glue 
+python test_mvbb_filtered.py paper_mate_12_count_mirado_black_warrior
+python test_mvbb_filtered.py large_black_spring_clamp
+python test_mvbb_filtered.py medium_black_spring_clamp
+python test_mvbb_filtered.py windex
+python test_mvbb_filtered.py play_go_rainbow_stakin_cups_3_red
+python test_mvbb_filtered.py play_go_rainbow_stakin_cups_5_green
+python test_mvbb_filtered.py wearever_cooking_pan_with_lid
+python test_mvbb_filtered.py stanley_13oz_hammer
+python test_mvbb_filtered.py stainless_steel_spoon_red_handle
+python test_mvbb_filtered.py stainless_steel_fork_red_handle
+python test_mvbb_filtered.py dr_browns_bottle_brush
