f2511c3adb8417a01f811540697296ac28537256
Manuel Bonilla <josemanuelbonilla@gmail.com>
2016-10-11 10:22:53 +0900

    Final scenario

---
+++ competition.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+####################################
+# python test_mvbb_filtered.py kygen_squeakin_eggs_plush_puppies 
+# python test_mvbb_filtered.py laugh_out_loud_joke_book 
+# python test_mvbb_filtered.py mark_twain_huckleberry_finn 
+python test_mvbb_filtered.py large_black_spring_clamp 
+python test_mvbb_filtered.py large_black_spring_clamp 
+python test_mvbb_filtered.py black_and_decker_lithium_drill_driver_unboxed 
+python test_mvbb_filtered.py cheezit_bit_original 
+python test_mvbb_filtered.py laugh_out_loud_joke_book 
+# python test_mvbb_filtered.py paper_mate_12_count_mirado_black_warrior 
+# python test_mvbb_filtered.py rollodex_mesh_collection_jumbo_pencil_cup 
+# python test_mvbb_filtered.py safety_works_safety_glasses 
+# python test_mvbb_filtered.py sharpie_accent_tank_style_highlighters 
+# python test_mvbb_filtered.py stanley_66_052 
+# python test_mvbb_filtered.py 1_and_a_half_in_metal_washer 
+# python test_mvbb_filtered.py 1in_metal_washer 
+# python test_mvbb_filtered.py 2in_metal_washer 
+# python test_mvbb_filtered.py black_and_decker_lithium_drill_driver 
+# python test_mvbb_filtered.py black_and_decker_lithium_drill_driver_unboxed 
+# python test_mvbb_filtered.py blank_hard_plastic_card 
+# python test_mvbb_filtered.py block_of_wood_12in 
+# python test_mvbb_filtered.py block_of_wood_6in 
+# python test_mvbb_filtered.py blue_wood_block_1inx1in 
+# python test_mvbb_filtered.py brine_mini_soccer_ball 
+# python test_mvbb_filtered.py campbells_condensed_tomato_soup 
+# python test_mvbb_filtered.py champion_sports_official_softball 
+# python test_mvbb_filtered.py cheerios_14oz 
+# python test_mvbb_filtered.py cheeze-it_388g 
+# python test_mvbb_filtered.py clorox_disinfecting_wipes_35 
+# python test_mvbb_filtered.py comet_lemon_fresh_bleach 
