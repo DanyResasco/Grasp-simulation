74727a6fbaaeb4513ecc10374d9a8c97bd38b326
Manuel Bonilla <josemanuelbonilla@gmail.com>
2016-10-11 03:18:52 +0200

    giorgio results

---
+++ build_database_giorgio2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+python test_mvbb_filtered.py champion_copper_plus_spark_plug 
+python test_mvbb_filtered.py cheezit_big_original 
+python test_mvbb_filtered.py crayola_64_ct 
+python test_mvbb_filtered.py dove_beauty_bar 
+python test_mvbb_filtered.py dr_browns_bottle_brush 
+python test_mvbb_filtered.py elmers_washable_no_run_school_glue 
+python test_mvbb_filtered.py expo_dry_erase_board_eraser 
+python test_mvbb_filtered.py feline_greenies_dental_treats 
+python test_mvbb_filtered.py first_years_take_and_toss_straw_cups 
+python test_mvbb_filtered.py genuine_joe_plastic_stir_sticks 
+python test_mvbb_filtered.py highland_6539_self_stick_notes 
+python test_mvbb_filtered.py kong_air_dog_squeakair_tennis_ball 
+python test_mvbb_filtered.py kong_sitting_frog_dog_toy 
+####################################
+
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_apple 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_banana 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_lemon 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_orange 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_peach 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_pear 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_plum 
+# python test_mvbb_filtered.py melissa_doug_farm_fresh_fruit_strawberry 
+# # python test_mvbb_filtered.py melissa_doug_play-time_produce_farm_fresh_fruit_unopened_box 
+# python test_mvbb_filtered.py morton_pepper_shaker 
+# python test_mvbb_filtered.py morton_salt_shaker 
