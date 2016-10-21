04c414d4241979aa0c3760ce14ed15cd0e125ee9
Alessio Rocchi <alessio.rocchi@iit.it>
2016-10-21 10:07:14 +0200

    Commit of last work on balls task

--- balls_controller.py
+++ balls_controller.py
@@ -7,6 +7,7 @@ from i16mc import box_dims
 from IPython import embed
 import numpy as np
 from copy import deepcopy
+from create_mvbb import get_bb_enclosed_sphere
 
 class BallsStateMachineController(object):
     """A more sophisticated controller that uses a state machine."""
@@ -35,10 +36,11 @@ class BallsStateMachineController(object):
         #print "State:",state
         t_park = 0.5
         t_lower = 0.5
+        t_pregrasp = 0.5
         t_grasp = 1.0
         t_lift = 0.5
         t_ungrasp = 0.5
-        t_wait = 2.0
+        t_wait = 1.0
         if self.state == 'wait':
             if sim.getTime() > t_wait:
                 self.state = 'idle'
@@ -84,11 +86,24 @@ class BallsStateMachineController(object):
                 self.t0 = sim.getTime()
                 #this is needed to stop at the current position in case there's some residual velocity
                 controller.setPIDCommand(controller.getCommandedConfig(),[0.0]*len(controller.getCommandedConfig()))
-                self.hand.setCommand([1.0])
+
                 self.state = 'grasping'
                 print self.state
 
         elif self.state == 'grasping':
+            u = (sim.getTime() - self.t0) / t_grasp
+            R_x = so3.from_axis_angle(((0, 1, 0), np.pi))
+            goal_angle = so3.mul(self.start_pose[0], R_x)
+            goal_pos = deepcopy(self.start_pose[1])
+            p, r = get_bb_enclosed_sphere(self.ball)
+            goal_pos[0] += r
+            goal_pose = (goal_angle, goal_pos)
+            T = se3.interpolate(self.start_pose, goal_pose, np.min((u, 1.0)))
+            send_moving_base_xform_PID(controller, T[0], T[1])
+
+            if sim.getTime() - self.t0 > t_pregrasp:
+                self.hand.setCommand([1.0])
+
             if sim.getTime() - self.t0 > t_grasp:
                 self.t0 = sim.getTime()
                 self.start_pose = get_moving_base_xform(self.sim.controller(0).model())
@@ -147,7 +162,7 @@ class BallsStateMachineController(object):
         for i in range(w.numRigidObjects()):
             o = w.rigidObject(i)
 
-            if 'sphere' in o.getName():
+            if 'ball' in o.getName():
                 R, t = o.getTransform()
                 if t[0] > -box_dims[0]/2.0 + 0.005 and t[0] < box_dims[0]/2.0 - 0.005 and \
                    t[1] > -box_dims[1]/2.0 + 0.005 and t[1] < box_dims[1]/2.0 - 0.005:
@@ -158,8 +173,10 @@ class BallsStateMachineController(object):
         return self.balls_to_move
 
     def _get_hand_pose_for_ball(self, ball):
+        p, r = get_bb_enclosed_sphere(ball)
         w_T_op = np.eye(4)
-        w_T_op[2,3] = 0.11 # TODO Manuel get this value from the BB or from the algorithm
+        w_T_op[0,3] = -r
+        w_T_op[2,3] =  -r
         """
         w_T_o = np.array(ball.getTransform())
         p_T_h = np.array(se3.homogeneous(self.p_T_h))
