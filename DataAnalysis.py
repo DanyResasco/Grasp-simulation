89eb14f3a006bdac1ac5b6dca85eb1417f6c23da
DanyResasco <daniela.resasco@gmail.com>
2016-11-20 23:25:49 -0500

    rewrite in compact format

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -22,28 +22,29 @@ class DataAnalysis():
     def __init__(self):
         self.n_simulation_total = 0
         self.n_simulation_obj = 1
-        self.index = 0
-        self.nome = ''
+        # self.index = 0
+        # self.nome = ''
         self.grasp_succesful = 0
         self.Mesh_data = []
         self.first_time = 0
-        self.grasp_status = ''
+        # self.grasp_status = ''
         self.kindness = ''
+        self.parameter = ''
     
     def Set_Name(self):
         if self.first_time is 0:
-            self.Nome_finito = self.nome
-            self.nome = ''
+            self.Nome_finito = self.parameter
+            self.parameter = ''
             self.first_time = 1
         else:
-            if self.Nome_finito == self.nome:
+            if self.Nome_finito == self.parameter:
                 self.n_simulation_obj += 1
-                self.nome = ''
+                self.parameter = ''
             else:
                 self.Mesh_data.append({'Name':self.Nome_finito,'n_simulation_obj':self.n_simulation_obj ,'grasp_succesful': self.grasp_succesful})
                 print self.Mesh_data
-                self.Nome_finito = self.nome
-                self.nome = ''
+                self.Nome_finito = self.parameter
+                self.parameter = ''
                 self.n_simulation_obj = 1
                 analysis.grasp_succesful = 0
 
@@ -51,27 +52,21 @@ class DataAnalysis():
         self.n_simulation_total +=1
 
     def Get_Kindness(self):
-        self.kindness += row[0][i]
-        if i is len(row[0]):
-            Draw_Grasph(self.kindness)
-            self.kindness = ''
-            return True
+        # Draw_Grasph(self.parameter)
+        self.kindness = self.parameter
+        self.parameter = ''
+        return True
 
     def Get_Grasp_Status(self):
-        if valuate is 1:
-            boolean = str2bool(self.grasp_status)
+        # if valuate is 1:
+            print self.parameter
+            boolean = str2bool(self.parameter)
             if boolean is 1:
                 self.grasp_succesful += 1
-                self.grasp_status = ''
+                self.parameter = ''
             else:
-                self.grasp_status = ''
+                self.parameter = ''
             return True
-        else:
-            self.grasp_status += row[0][i]
-            print self.grasp_status
-            return False
-
-
 
 import csv
 
@@ -88,24 +83,47 @@ with open(obj_dataset, 'rb') as csvfile:
         for i in range(0,len(row[0])):
             if row[0][i] is',':
                 count_comma +=1
-                # print "n_comma: ", count_comma
                 if count_comma is 1:
                     analysis.Set_Name()
-                if count_comma is 13: # inizio a leggere grasp status
-                    active_grasp_evaluate = 1
+                elif count_comma <= 13:
+                    analysis.parameter = ''
                 if count_comma is 14: #finito di leggere grasp status e inizio con kindness
-                    valuate = 1
-
+                    analysis.Get_Grasp_Status()
             else:
-                if count_comma < 1:
-                    analysis.nome += row[0][i]
-                if active_grasp_evaluate is 1:
-                    print "entro if"
-                    stop = analysis.Get_Grasp_Status()
-                    if stop:
-                        count_comma = 0
-                        active_grasp_evaluate = 0
-                        print "qui"
-                        stop = False
-                        valuate = 0
-                        break
+                analysis.parameter += row[0][i]
+                if count_comma is 14:
+                    analysis.Get_Kindness()
+                    count_comma = 0
+                    break
+
+
+
+
+
+
+    # for row in file_reader:
+    #     analysis.Set_Nsimulation()
+    #     for i in range(0,len(row[0])):
+    #         if row[0][i] is',':
+    #             count_comma +=1
+    #             # print "n_comma: ", count_comma
+    #             if count_comma is 1:
+    #                 analysis.Set_Name()
+    #             if count_comma is 13: # inizio a leggere grasp status
+    #                 active_grasp_evaluate = 1
+    #             if count_comma is 14: #finito di leggere grasp status e inizio con kindness
+    #                 valuate = 1
+
+    #         else:
+    #             if count_comma < 1:
+    #                 analysis.nome += row[0][i]
+    #             if active_grasp_evaluate is 1:
+    #                 print "entro if"
+    #                 stop = analysis.Get_Grasp_Status()
+    #                 if stop:
+    #                     count_comma = 0
+    #                     active_grasp_evaluate = 0
+    #                     print "qui"
+    #                     stop = False
+    #                     valuate = 0
+    #                     break
