b9085b663b32390987e1693c423f1cc0f3e703ea
DanyResasco <daniela.resasco@gmail.com>
2016-11-22 09:53:58 -0500

    change the file

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,7 +1,6 @@
 import numpy as np
-# from mvbb.dataAnalysislog import DanyDataAnalysis
-obj_dataset = 'db/testdataanalysis.csv'
-# obj_dataset = 'db/database_scoredreflex_DanyFinito.csv'
+
+res_dataset = 'db/Results.csv'
 
 def str2bool(st):
     try:
@@ -29,85 +28,64 @@ class DataAnalysis():
         self.grasp_status = ''
         self.kindness = []
         self.parameter = ''
-        # self.datares = DanyDataAnalysis(suffix='results')
+
     def Save_Results(self):
         import csv
-        with open('db/dataAnalysislog.csv', 'wb') as csvfilereader:
+        print "save"
+        with open(res_dataset, 'wb') as csvfilereader:
+            print "salvo"
             writer = csv.writer(csvfilereader, delimiter=' ',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for i in self.Mesh_data:
                 writer.writerow([i])
             csvfilereader.close()
 
-
-
-    def Set_Name(self):
+    def Set_Name(self, obj_name):
         if self.first_time is 0:
-            self.Nome_finito = self.parameter
-            self.parameter = ''
+            self.Nome_finito = obj_name
             self.first_time = 1
         else:
-            if self.Nome_finito == self.parameter:
+            if self.Nome_finito == obj_name:
                 self.n_simulation_obj += 1
-                self.parameter = ''
             else:
                 self.Mesh_data.append({'Name':self.Nome_finito,'n_simulation_obj':self.n_simulation_obj ,
                 'grasp_succesful': self.grasp_succesful,'Percentage': (self.grasp_succesful / float(self.n_simulation_obj))*100})
-                self.Nome_finito = self.parameter
-                self.parameter = ''
+                self.Nome_finito = obj_name
                 self.n_simulation_obj = 1
                 analysis.grasp_succesful = 0
 
     def Set_Nsimulation(self):
         self.n_simulation_total +=1
 
-    def Get_Kindness(self, end_file):
-        self.kindness.append(self.parameter)
+    def Get_Kindness(self, value, end_file):
+        self.kindness.append(value)
         if end_file:
+                print "dentro end_file"
                 # Draw_Grasph(self.kindness)
                 self.Mesh_data.append({'Name':self.Nome_finito,'n_simulation_obj':self.n_simulation_obj ,
                     'grasp_succesful': self.grasp_succesful,'Percentage': (self.grasp_succesful / float(self.n_simulation_obj))*100})
                 self.Save_Results()
-        self.parameter = ''
         return True
 
-    def Get_Grasp_Status(self):
-        boolean = str2bool(self.parameter)
+    def Get_Grasp_Status(self,status):
+        boolean = str2bool(status)
         if boolean is 1:
             self.grasp_succesful += 1
-            self.parameter = ''
-        else:
-            self.parameter = ''
-        return True
+
 
 import csv
-
+import sys
+obj_dataset = sys.argv[1]
 with open(obj_dataset, 'rb') as csvfile:
-    file_reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    file_reader = csv.reader(csvfile, delimiter=',')
     analysis = DataAnalysis()
     count_comma = 0
     N_row_total = sum(1 for i in file_reader)
-    csvfile.seek(0)
     N_row_count = 0
-    count_par = 0
+    print N_row_total
+    csvfile.seek(0)
     for row in file_reader:
-        N_row_count +=1
-        analysis.Set_Nsimulation()
-        n_par = len(range(0,len(row[0])))
-        for i in range(0,len(row[0])):
-            count_par += 1
-            if row[0][i] is',':
-                count_comma +=1
-                if count_comma is 1:
-                    analysis.Set_Name()
-                elif count_comma <= 13:
-                    analysis.parameter = ''
-                if count_comma is 14: #finito di leggere grasp status e inizio con kindness
-                    analysis.Get_Grasp_Status( )
-            else:
-                analysis.parameter += row[0][i]
-                if count_comma is 14 and count_par is n_par:
-                    analysis.Get_Kindness(True if N_row_count == N_row_total else False)
-                    count_comma = 0
-                    count_par = 0
-                    break
+        N_row_count += 1
+        analysis.Set_Name(row[0])
+        analysis.Get_Grasp_Status(row[13])
+        analysis.Get_Kindness(row[14],True if N_row_count == N_row_total else False)
