5f4a4d6ce25343eeec8bad15949cbd6bcfeedfaa
DanyResasco <daniela.resasco@gmail.com>
2017-02-13 14:30:07 -0500

    camera and robot work. not image creation

--- prova_camera.py
+++ prova_camera.py
@@ -10,21 +10,26 @@ from IPython import embed
 from i16mc import make_object, make_moving_base_robot
 import csv
 import numpy as np
+import math
+import os
+import string
+import sys
 from moving_base_control import *
 # from Add_variation_camera_pose import Make_camera_poses
 import scipy.misc
 from Add_variation_camera_pose import Add_variation,Make_camera_poses
-from utils_camera import FromCamera2rgb, Find_axis_rotation
+from utils_camera import FromCamera2rgb, Find_axis_rotation, Read_Poses, Write_image
+from mvbb.ScalaReduce import DanyReduceScale
 
-def Read_Poses(nome,vector_set):
 
-    obj_dataset = '3DCNN/NNSet/Pose/pose/%s.csv'%(nome)
-    with open(obj_dataset, 'rb') as csvfile: #open the file in read mode
-        file_reader = csv.reader(csvfile,quoting=csv.QUOTE_NONNUMERIC)
-        for row in file_reader:
-            T = row[9:12]
-            pp = row[:9]
-            vector_set.append( np.array((pp,T))) 
+Pose = {}
+Pose['pose'] = [f for f in os.listdir('3DCNN/NNSet/Pose/pose')]
+objects = {}
+objects['ycb'] = [f for f in os.listdir('data/objects/ycb')]
+objects['apc2015'] = [f for f in os.listdir('data/objects/apc2015')]
+# objects['newObjdany'] = [f for f in os.listdir('data/objects/newObjdany')]
+objects['princeton'] = [f for f in os.listdir('data/objects/princeton')]
+
 
 
 
@@ -45,126 +50,157 @@ def processDepthSensor(sensor):
             maxd = max(depth,maxd)
     print "Depth range",mind,maxd
 
+
 world = WorldModel()
-world.readFile("data/robots/test_sensor.xml")
+# world.readFile("data/robots/test_sensor.xml")
 world.loadElement("data/terrains/plane.env")
 # embed()
 # robot = world.robot(0)
 # robot = make_moving_base_robot("reflex_col", world)
 # xform = resource.get("default_initial_reflex_col.xform" , description="Initial hand transform",default=se3.identity(), world=world, doedit=False)
-# # pose_se3= ([0.983972802704,-0.0441290216922,0.172771968165,0.177866245396,0.173919267423,-0.968563723855,0.0126933954459,0.983770663246,0.178980892412],[0.234435798004,0.0102866113634,0.0952616290142])
-# set_moving_base_xform(robot,so3.identity(),[0,0,-8])
+# pose_se3= ([0.983972802704,-0.0441290216922,0.172771968165,0.177866245396,0.173919267423,-0.968563723855,0.0126933954459,0.983770663246,0.178980892412],[0.234435798004,0.0102866113634,0.0952616290142])
+# set_moving_base_xform(robot,xform[0],[0,0,-1])
+# robot_config = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,-1]
+# embed()
+robot = make_moving_base_robot('reflex_col', world)
+xform = resource.get("default_initial_reflex_col.xform" , description="Initial hand transform",
+                         default=se3.identity(), world=world, doedit=False)
+set_moving_base_xform(robot,xform[0],[0,0,-1])
+# q = robot.getConfig()
+# q[0] = 0.05
+# q[1] = 0.01
+# q[2] = -1
+# q[3] = 0 #yaw
+# q[4] = 0 #pitch
+# q[5] = math.radians(180) #roll
+# robot.setConfig(q)
+# robot.setConfig((robot_config))
+
+
+all_objects = []
+object_list = []
+for dataset in objects.values():
+    all_objects += dataset
+
+to_check = []
+done =[]
+to_filter=[]
+to_do=[]
+
+for obj_name in to_filter + to_do + done + to_check:
+    all_objects.pop(all_objects.index(obj_name))
+
+# print "-------------"
+# print all_objects
+# print "-------------"
+
+try:
+    object_list.append(sys.argv[1])
+    # MainDany([objname])
+except:
+    object_list = (all_objects)
 
 
 
 
-obj = make_object('princeton', 'xwing', world)
+
+for object_name in object_list:
+    obj = None
+    for object_set, objects_in_set in objects.items():
+        if object_name in objects_in_set:
+            for i,t in Pose.items():
+                if (object_name +'.csv') in t:
+                    if world.numRigidObjects() > 0:
+                        world.remove(world.rigidObject(0))
+                    if object_name in objects['princeton']:
+                        print "*************Dentro princeton********************" #need to scale the obj size
+                        objfilename = 'data/objects/template_obj_scale_princeton.obj'
+                        print"objfilename", objfilename
+                        obj = DanyReduceScale(object_name, world,objfilename,object_set)
+                    else:
+                        obj = make_object(object_set, object_name, world)
+                if obj is None:
+                    print "Could not find object", object_name
+                    continue
+
+                o_T_p= []
+                 # = []
+                Read_Poses(object_name,o_T_p)
+                o_T_p_r = Make_camera_poses(o_T_p,obj)
+
+                vis.add("world",world)
+
+                sim = Simulator(world)
+                sensor = sim.controller(0).sensor("rgbd_camera")
+                print"LINK", sensor.getSetting("link")
+                print "Tsensor", sensor.getSetting("Tsensor")
 
 
-o_T_p= []
- # = []
-Read_Poses('xwing',o_T_p)
-o_T_p_r = Make_camera_poses(o_T_p,obj)
 
-vis.add("world",world)
+                #Note: GLEW sensor simulation only runs if it occurs in the visualization thread (e.g., the idle loop)
+                class SensorTestWorld (GLPluginInterface):
+                    def __init__(self,poses,world,object_name):
+                        self.p = 0
+                        self.poses = poses
+                        self.world = world
+                        self.is_simulating = False
+                        self.curr_pose = None
+                        self.running = True
+                        self.obj = obj
+                        self.curr_pose = None
+                        # self.sim = None
+                        self.step = 0
+                        # self.camera = None
+                        self.nome_obj = object_name
+                        self.t_0 = None
+                        self.simulation_ = None
+                        # robot.randomizeConfig()
+                        #sensor.kinematicSimulate(world,0.01)
+                        # sim.controller(0).setPIDCommand(robot.getConfig(),[0.0]*7)
 
-sim = Simulator(world)
-sensor = sim.controller(0).sensor("rgbd_camera")
-print"LINK", sensor.getSetting("link")
-print "Tsensor", sensor.getSetting("Tsensor")
+                    def idle(self):
+                        print "Idle..."
+                        if not self.running:
+                            return
 
-def Write_image(camera,dataset):
-    '''Write the dataset'''
-    # embed()
-    import csv
-    f = open(dataset, 'w')
-    # embed()
-    for i in camera:
-        f.write(','.join([str(i)]))
-        f.write('\n')
-    f.close()
+                        if not self.is_simulating:
+                            if len(self.poses) > 0:
+                                self.curr_pose = self.poses.pop(0)
 
-    #Note: GLEW sensor simulation only runs if it occurs in the visualization thread (e.g., the idle loop)
-class SensorTestWorld (GLPluginInterface):
-    def __init__(self,poses,world,object_name):
-        self.p = 0
-        self.poses = poses
-        self.world = world
-        self.is_simulating = False
-        self.curr_pose = None
-        self.running = True
-        self.obj = obj
-        self.curr_pose = None
-        # self.sim = None
-        self.step = 0
-        # self.camera = None
-        self.nome_obj = object_name
-        self.t_0 = None
-        self.simulation_ = None
-        # robot.randomizeConfig()
-        #sensor.kinematicSimulate(world,0.01)
-        # sim.controller(0).setPIDCommand(robot.getConfig(),[0.0]*7)
+                                # print  self.curr_pose
+                                print len(self.poses)
+                            else:
+                                self.running = False
+                                vis.show(hidden=True)
+                                return
 
-    def idle(self):
-        print "Idle..."
-        #this uses the simulation
-        # sim.simulate(0.1)
-        # sim.updateWorld()
-        # # time.sleep(0.1)
-        # #this commented out line just uses the world and kinematic simulation
-        # #sensor.kinematicSimulate(world,0.01)
-        # processDepthSensor(sensor)
-        # camera_measure = sensor.getMeasurements()
-        # rgb = FromCamera2rgb(camera_measure)
-        # scipy.misc.imsave('outfile_%s.jpg'%self.p, rgb)
-        # res_dataset = '2DCNN/NNSet/Image/%s_rotate_%s.csv'% ('xwing',self.p)
-        # Write_image(camera_measure,res_dataset)
-        # self.p +=1
-        #         # o_T_p.pop(0)
-        # return True
-        if not self.running:
-            return
+                            if self.simulation_ is None:
+                                vis.add("world",self.world)
+                                
+                            self.t_0 = sim.getTime()
+                            self.is_simulating = True
 
-        if not self.is_simulating:
-            if len(self.poses) > 0:
-                self.curr_pose = self.poses.pop(0)
+                        if self.is_simulating:
+                            sensor.setSetting("Tsensor",' '.join(str(v) for v in self.curr_pose[0]+self.curr_pose[1]))
+                            vis.add("sensor",sensor)
+                            
+                            sim.simulate(0.1)
+                            sim.updateWorld()
 
-                # print  self.curr_pose
-                print len(self.poses)
-            else:
-                self.running = False
-                vis.show(hidden=True)
-                return
-
-            if self.simulation_ is None:
-                vis.add("world",self.world)
-                
-            self.t_0 = sim.getTime()
-            self.is_simulating = True
-
-        if self.is_simulating:
-            sensor.setSetting("Tsensor",' '.join(str(v) for v in self.curr_pose[0]+self.curr_pose[1]))
-            vis.add("sensor",sensor)
-            
-
-            # self.camera.setSetting("Tsensor",Tsensor)
-            sim.simulate(0.1)
-            sim.updateWorld()
-
-            if not vis.shown() or (sim.getTime() - self.t_0) >= 2.5:
-                if  vis.shown():
-                    camera_measure = sensor.getMeasurements()
-                    rgb = FromCamera2rgb(camera_measure)
-                    scipy.misc.imsave('outfile_%s.jpg'%self.step, rgb)
-                    res_dataset = '2DCNN/NNSet/Image/%s_rotate_%s.csv'% (self.nome_obj,self.step)
-                    Write_image(camera_measure,res_dataset)
-                    self.step +=1
-                    self.is_simulating = False
-                    self.simulation_  = None
+                            if not vis.shown() or (sim.getTime() - self.t_0) >= 2.5:
+                                if  vis.shown():
+                                    camera_measure = sensor.getMeasurements()
+                                    rgb = FromCamera2rgb(camera_measure)
+                                    scipy.misc.imsave('outfile_%s.jpg'%self.step, rgb)
+                                    res_dataset = '2DCNN/NNSet/Image/%s_rotate_%s.csv'% (self.nome_obj,self.step)
+                                    Write_image(camera_measure,res_dataset)
+                                    self.step +=1
+                                    self.is_simulating = False
+                                    self.simulation_  = None
 
 
-vis.pushPlugin(SensorTestWorld(o_T_p_r,world,'xwing'))
-vis.show()
-while vis.shown():
-    time.sleep(0.5)
-vis.kill()
+                vis.pushPlugin(SensorTestWorld(o_T_p_r,world,'xwing'))
+                vis.show()
+                while vis.shown():
+                    time.sleep(0.5)
+                vis.kill()
