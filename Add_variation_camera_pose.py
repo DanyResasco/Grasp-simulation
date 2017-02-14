fixed rotation in make_variation

Add_variation_camera_pose.py
@@ -24,7 +24,7 @@ def Add_variation(o_T_p_r_original,o_T_p_r):
 
     ##Variation on x
     R = np.array(se3.homogeneous((so3.from_axis_angle(([1,0,0],theta)),[0,0,0] )))
-    o_T_p_r_temp = se3.from_homogeneous(( np.dot(R,np.array(se3.homogeneous(o_T_p_r_original)))))
+    o_T_p_r_temp = se3.from_homogeneous(( np.dot(np.array(se3.homogeneous(o_T_p_r_original),R ))))
     o_T_p_r_temp[1][0:3] = o_T_p_r_original[1][0:3]
     # o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - 0.7
     o_T_p_r.append(o_T_p_r_temp)
@@ -32,7 +32,7 @@ def Add_variation(o_T_p_r_original,o_T_p_r):
 
     #  #variazione su y
     R = np.array(se3.homogeneous((so3.from_axis_angle(([0,1,0],theta)),[0,0,0] )))
-    o_T_p_r_temp = se3.from_homogeneous(( np.dot(R,np.array(se3.homogeneous(o_T_p_r_original)))))
+    o_T_p_r_temp = se3.from_homogeneous(( np.dot(np.array(se3.homogeneous(o_T_p_r_original),R))))
     o_T_p_r_temp[1][0:3] = o_T_p_r_original[1][0:3]
     # o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - 0.7
     o_T_p_r.append(o_T_p_r_temp)
@@ -40,7 +40,7 @@ def Add_variation(o_T_p_r_original,o_T_p_r):
     # #variazione su x e muovo su z
     move_z = random.randrange(-5,5)
     R = np.array(se3.homogeneous((so3.from_axis_angle(([1,0,0],theta)),[0,0,0] )))
-    o_T_p_r_temp = se3.from_homogeneous(( np.dot(R,np.array(se3.homogeneous(o_T_p_r_original)))))
+    o_T_p_r_temp = se3.from_homogeneous(( np.dot(np.array(se3.homogeneous(o_T_p_r_original),R))))
     o_T_p_r_temp[1][0:3] = o_T_p_r_original[1][0:3]
     o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - move_z*0.01
     # o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - 0.7
@@ -49,7 +49,7 @@ def Add_variation(o_T_p_r_original,o_T_p_r):
     # #variazione su y e muovo su z
     move_z = random.randrange(-5,5)
     R = np.array(se3.homogeneous((so3.from_axis_angle(([0,1,0],theta)),[0,0,0] )))
-    o_T_p_r_temp = se3.from_homogeneous(( np.dot(R,np.array(se3.homogeneous(o_T_p_r_original)))))
+    o_T_p_r_temp = se3.from_homogeneous(( np.dot(np.array(se3.homogeneous(o_T_p_r_original),R))))
     # o_T_p_r_temp[1][0:3] = o_T_p_r_original[1][0:3]
     o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - move_z*0.01
     # o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - 0.7
@@ -72,7 +72,7 @@ def Add_variation(o_T_p_r_original,o_T_p_r):
     #  #variazione su y e muovo su z
     move_z = random.randrange(-5,5)
     R = np.array(se3.homogeneous((so3.from_axis_angle(([0,1,0],theta)),[0,0,0] )))
-    o_T_p_r_temp = se3.from_homogeneous(( np.dot(R,np.array(se3.homogeneous(o_T_p_r_original)))))
+    o_T_p_r_temp = se3.from_homogeneous(( np.dot(np.array(se3.homogeneous(o_T_p_r_original),R))))
     o_T_p_r_temp[1][0:3] = o_T_p_r_original[1][0:3]
     o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - move_z*0.01
     # o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - 0.7
@@ -81,7 +81,7 @@ def Add_variation(o_T_p_r_original,o_T_p_r):
     #  #variazione su x e muovo su y
     move_z = random.randrange(-5,5)
     R = np.array(se3.homogeneous((so3.from_axis_angle(([1,0,0],theta)),[0,0,0] )))
-    o_T_p_r_temp = se3.from_homogeneous(( np.dot(R,np.array(se3.homogeneous(o_T_p_r_original)))))
+    o_T_p_r_temp = se3.from_homogeneous(( np.dot(np.array(se3.homogeneous(o_T_p_r_original),R))))
     o_T_p_r_temp[1][0:3] = o_T_p_r_original[1][0:3]
     o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - move_z*0.01
     # o_T_p_r_temp[1][1] = o_T_p_r_original[1][1] - 0.7