02eaac16a9a97e59f2451bbac3a7833422e62093
DanyResasco <daniela.resasco@gmail.com>
2017-03-03 15:44:41 -0500

    nevterov function and res

--- 2DCNN/Main.py
+++ 2DCNN/Main.py
@@ -11,6 +11,10 @@ from cont_output_layer import ContOutputLayer
 from IPython import embed
 from dataset_Dany import Input_output 
 
+# import theano
+from collections import OrderedDict
+# import theano.tensor as T
+
 
 def Draw_Grasph(truth,prediction,eo,et):
     print "disegno"
@@ -53,7 +57,7 @@ def save_model(filename, **layer_dict):
 '''param rng: a random number generator used to initialize weights'''
 random.seed(0)
 rng = np.random.RandomState(65432)
-batch_size = 8
+batch_size = 10
 nkerns = (10, 10, 10, 10, 10, 10) # n felter for each layer
 
 # batch_size = 6
@@ -73,24 +77,53 @@ print 'defining architecture'
 ''' image_shape is (num_imgs, num_channels, img_height, img_width, img_length)
 	filter_shape is (num_kernels, num_channels, kernel_height, kernel_width, kernel_length)
 	size(image_shape[num_channels]) == size(filter_shape[num_channels]) '''
-#(64-7+1)/poolsize
+#(64-7+`)/poolsize
+# conv1 = Conv2D(rng, input=input_occ_batch, filter_shape=(nkerns[0], 3, 7, 7), 
+#     image_shape=(batch_size, 3, 256, 256), poolsize=(1,1))
+# conv2 = Conv2D(rng, input=conv1.output, filter_shape=(nkerns[1], nkerns[0], 7, 7), 
+#     image_shape=(batch_size, nkerns[0], 250,250), poolsize=(1,1))
+# conv3 = Conv2D(rng, input=conv2.output, filter_shape=(nkerns[2], nkerns[1], 5, 5), 
+#     image_shape=(batch_size, nkerns[1], 244, 244), poolsize=(2,2))
+
+
+# conv4 = Conv2D(rng, input=conv3.output, filter_shape=(nkerns[3], nkerns[2], 5, 5), 
+#     image_shape=(batch_size, nkerns[2], 120, 120), poolsize=(2,2))
+
+
+# conv5 = Conv2D(rng, input=conv4.output, filter_shape=(nkerns[4], nkerns[3], 5, 5), 
+#     image_shape=(batch_size, nkerns[3], 58, 58), poolsize=(1,1))
+
+# # conv6 = Conv2D(rng, input=conv5.output, filter_shape=(nkerns[5], nkerns[4], 3, 3), 
+# #   image_shape=(batch_size, nkerns[4], 8, 8), poolsize=(1,1))
+
+
+# fc_input = conv5.output.flatten(2)
+# fc1 = FullyConnectedLayer(rng, input=fc_input, n_in=54*54*nkerns[4], n_out=5500)
+# #output is a vector of 12 elements
+# fc2 = FullyConnectedLayer(rng, input=fc1.output, n_in=5500, n_out=2500)
+# fc3 = FullyConnectedLayer(rng, input=fc2.output, n_in=2500, n_out=1500)
+# output = ContOutputLayer(input=fc3.output, n_in =1500 ,n_out=6)
+
+
+
+
 conv1 = Conv2D(rng, input=input_occ_batch, filter_shape=(nkerns[0], 1, 5, 5), 
-	image_shape=(batch_size, 1, 256, 256), poolsize=(1,1))
+    image_shape=(batch_size, 1, 256, 256), poolsize=(1,1))
 conv2 = Conv2D(rng, input=conv1.output, filter_shape=(nkerns[1], nkerns[0], 5, 5), 
- 	image_shape=(batch_size, nkerns[0], 252,252), poolsize=(1,1))
+    image_shape=(batch_size, nkerns[0], 252,252), poolsize=(1,1))
 conv3 = Conv2D(rng, input=conv2.output, filter_shape=(nkerns[2], nkerns[1], 5, 5), 
- 	image_shape=(batch_size, nkerns[1], 248, 248), poolsize=(2,2))
+    image_shape=(batch_size, nkerns[1], 248, 248), poolsize=(2,2))
 
 
 conv4 = Conv2D(rng, input=conv3.output, filter_shape=(nkerns[3], nkerns[2], 3, 3), 
-	image_shape=(batch_size, nkerns[2], 122, 122), poolsize=(2,2))
+    image_shape=(batch_size, nkerns[2], 122, 122), poolsize=(2,2))
 
 
 conv5 = Conv2D(rng, input=conv4.output, filter_shape=(nkerns[4], nkerns[3], 3, 3), 
-	image_shape=(batch_size, nkerns[3], 60, 60), poolsize=(2,2))
+    image_shape=(batch_size, nkerns[3], 60, 60), poolsize=(2,2))
 
 conv6 = Conv2D(rng, input=conv5.output, filter_shape=(nkerns[5], nkerns[4], 4, 4), 
-	image_shape=(batch_size, nkerns[4], 29, 29), poolsize=(2,2))
+    image_shape=(batch_size, nkerns[4], 29, 29), poolsize=(2,2))
 
 
 fc_input = conv6.output.flatten(2)
@@ -100,15 +133,31 @@ fc2 = FullyConnectedLayer(rng, input=fc1.output, n_in=5500, n_out=2500)
 fc3 = FullyConnectedLayer(rng, input=fc2.output, n_in=2500, n_out=1500)
 output = ContOutputLayer(input=fc3.output, n_in =1500 ,n_out=6)
 
+
+
+
+
+
+
+
+
+
+
+
+
 print 'defining cost'
 
-cost = output.dany_error(y,batch_size)
+# cost = output.dany_error(y,batch_size)
+cost = output.dany_error_std(y,batch_size)
+# cost = output.cost(y)
 
-all_params = (conv1.params + conv2.params + conv3.params + conv4.params + conv5.params +  conv6.params +fc1.params + fc2.params +fc3.params+ output.params)
+
+all_params = (conv1.params + conv2.params + conv3.params + conv4.params + conv5.params + conv6.params+  fc1.params + fc2.params +fc3.params + output.params)
 
   # compute the gradient of cost
 # embed()
 all_grads = T.grad(cost[0], all_params)
+# print all_grads
 print "grad"
 
 print 'defining train model'
@@ -119,9 +168,9 @@ train_set_X_occ, train_set_y = Dataset_dany[0]
 valid_set_x, valid_set_y = Dataset_dany[1]
 test_set_X_occ, test_set_y = Dataset_dany[2]
 
-# embed()
 
-#
+
+
 
 
 print train_set_y.type
@@ -129,25 +178,41 @@ print y.type
 print train_set_X_occ.type
 print X_occ.type
 print 'Adam Optimizer Update'
+
+
+# embed()
 # Adam Optimizer Update
-updates = []
-one = np.float32(1)
-zero = np.float32(0)
-adam_a=np.float32(0.0001); adam_b1=np.float32(0.1); adam_b2=np.float32(0.001); adam_e=np.float32(1e-8)
-adam_i = theano.shared(zero.astype(theano.config.floatX)) # iteration
-adam_i_new = adam_i + one # iteration update
-updates.append((adam_i, adam_i_new))
-adam_const1 = one - (one - adam_b1)**adam_i_new
-adam_const2 = one - (one - adam_b2)**adam_i_new
-for p, g in zip(all_params, all_grads):
-	adam_m = theano.shared(p.get_value() * zero)
-	adam_v = theano.shared(p.get_value() * zero)
-	adam_m_new = adam_b1 * g + ((one - adam_b1) * adam_m)
-	adam_v_new = (adam_b2 * T.sqr(g)) + ((one - adam_b2) * adam_v)
-	adam_p_new = p - adam_a * T.sqrt(adam_const2) / adam_const1* adam_m_new / (T.sqrt(adam_v_new) + adam_e)
-	updates.append((adam_m, adam_m_new))
-	updates.append((adam_v, adam_v_new))
-	updates.append((p, adam_p_new))
+# updates = []
+# one = np.float32(1)
+# zero = np.float32(0)
+# adam_a=np.float32(0.0001); adam_b1=np.float32(0.1); adam_b2=np.float32(0.001); adam_e=np.float32(1e-10)
+# adam_a=np.float32(0.00001); adam_b1=np.float32(0.1); adam_b2=np.float32(0.00001); adam_e=np.float32(1e-10)
+# adam_i = theano.shared(zero.astype(theano.config.floatX)) # iteration
+# adam_i_new = adam_i + one # iteration update
+# updates.append((adam_i, adam_i_new))
+# adam_const1 = one - (one - adam_b1)**adam_i_new
+# adam_const2 = one - (one - adam_b2)**adam_i_new
+# for p, g in zip(all_params, all_grads):
+# 	adam_m = theano.shared(p.get_value() * zero)
+# 	adam_v = theano.shared(p.get_value() * zero)
+# 	adam_m_new = adam_b1 * g + ((one - adam_b1) * adam_m)
+# 	adam_v_new = (adam_b2 * T.sqr(g)) + ((one - adam_b2) * adam_v)
+# 	adam_p_new = p - adam_a * T.sqrt(adam_const2) / adam_const1* adam_m_new / (T.sqrt(adam_v_new) + adam_e)
+# 	updates.append((adam_m, adam_m_new))
+# 	updates.append((adam_v, adam_v_new))
+# 	updates.append((p, adam_p_new))
+
+# all_grads = theano.grad(loss_or_grads, params)
+from updates import adamax,adam,adadelta,nesterov_momentum
+# updates = adam(all_grads, all_params, learning_rate=0.001, beta1=0.1,
+#          beta2=0.001, epsilon=1e-9)
+updates = adamax(all_grads, all_params, learning_rate=0.002, beta1=0.01,
+           beta2=0.0001, epsilon=1e-8)
+updates =adadelta(all_grads, all_params, learning_rate=1.0, rho=0.95, epsilon=1e-6)
+updates = nesterov_momentum(all_grads, all_params, 0.0002, momentum=0.9)
+
+
+
 
 ''' compiling a Theano function `train_model` that returns the cost, but
     in the same time updates the parameter of the model based on the rules
@@ -161,9 +226,9 @@ train_model = theano.function( [index], cost, updates=updates,
 )
 
 print 'defining test model'
-test_model = theano.function(	[index],	cost,
+test_model = theano.function(	[index],cost,
 	givens={
-		X_occ: test_set_X_occ[index * batch_size: (index + 1) * batch_size], #Scorre la matrice? 
+		X_occ: test_set_X_occ[index * batch_size: (index + 1) * batch_size], 
 		y: test_set_y[index * batch_size: (index + 1) * batch_size]
 	}
 )
@@ -206,7 +271,7 @@ start_time = timeit.default_timer()
 
 epoch = 0
 done_looping = False
-n_epochs =1000
+n_epochs =2560
 print 'prima del while'
 
 Truth = []
@@ -221,7 +286,10 @@ while (epoch < n_epochs) and (not done_looping):
     #     train_model(minibatch_index)
     #     # iteration number
     for minibatch_index in range(n_train_batches): #loop on train examples
-        train_model(minibatch_index)
+        a= train_model(minibatch_index)
+        # print a[0]
+        # print a[2]
+        # print a[2]
         # print op
         iter = (epoch - 1) * n_train_batches + minibatch_index
         if (iter + 1) % validation_frequency == 0:
@@ -285,17 +353,21 @@ while (epoch < n_epochs) and (not done_looping):
         if patience <= iter:
             print 'save'
             done_looping = True
-            res_name = '2d6Cnn3fcl_8_6_10_again.npz'
+            res_name = '2d6Cnn3fcl_8_5_10_again.npz'
             # save_model(res_name, conv1=conv1, conv2=conv2, conv3=conv3,conv4=conv4,
             # conv5=conv5,conv6=conv6, fc1=fc1, fc2=fc2,fc3=fc3, output=output)
             save_model(res_name, conv1=conv1, conv2=conv2, conv3=conv3,conv4=conv4,
-            conv5=conv5,conv6=conv6, fc1=fc1, fc2=fc2,fc3=fc3, output=output)
+            conv5=conv5,conv6=conv6, fc1=fc1, fc2=fc2,fc3=fc3 ,output=output)
             break
 
 end_time = timeit.default_timer()
 print(('Optimization complete. Best validation score of %f '
 	'obtained at iteration %i, with test performance %f ') %
 	(best_validation_loss , best_iter + 1, test_score ))
-# for i in Truth.value
-# embed()
+print 'save'
+res_name = '2d6Cnn3fcl_8_5_10_again.npz'
+# save_model(res_name, conv1=conv1, conv2=conv2, conv3=conv3,conv4=conv4,
+# conv5=conv5,conv6=conv6, fc1=fc1, fc2=fc2,fc3=fc3, output=output)
+save_model(res_name, conv1=conv1, conv2=conv2, conv3=conv3,conv4=conv4,
+conv5=conv5,conv6=conv6, fc1=fc1, fc2=fc2,fc3=fc3, output=output)
 Draw_Grasph(Truth,pred,E_ori,E_tra)
--- 2DCNN/cont_output_layer.py
+++ 2DCNN/cont_output_layer.py
@@ -1,11 +1,12 @@
 
 
-from __future__ import print_function, division
+from __future__ import division
 
 import numpy as np
 
 import theano
 import theano.tensor as T
+from IPython import embed
 
 
 class ContOutputLayer(object):
@@ -57,6 +58,95 @@ class ContOutputLayer(object):
 
 
 
+	def standardization(self,y,nrow):
+
+		roll_std  = T.std(y[:,0])
+		roll_mean = T.mean(y[:,0])
+		pitch_std  = T.std(y[:,1])
+		pitch_mean = T.mean(y[:,1])
+		yaw_std  = T.std(y[:,2])
+		yaw_mean = T.mean(y[:,2])
+
+		x_std = T.std(y[:,3])
+		x_mean = T.mean(y[:,3])
+		y_std = T.std(y[:,4])
+		y_mean = T.mean(y[:,4])
+		z_std = T.std(y[:,5])
+		z_mean = T.mean(y[:,5])
+
+
+
+
+		std_ = [roll_std,pitch_std,yaw_std,x_std,y_std,z_std]
+		mean_ = [roll_mean,pitch_mean,yaw_mean,x_mean,y_mean,z_mean]
+
+		# y_std = np.empty([nrow,6])
+
+		vect_temp = []
+		for i in range(0,nrow):
+			r= T.scalar() 
+			p= T.scalar()
+			w= T.scalar()
+			x= T.scalar()
+			v = T.scalar()
+			z = T.scalar()
+
+			r = (y[i,0] - std_[0])//mean_[0]
+			p = (y[i,1] - std_[1])//mean_[1]
+			w = (y[i,2] - std_[2])//mean_[2]
+			x = (y[i,3] - std_[3])//mean_[3]
+			v = (y[i,4] - std_[4])//mean_[4]
+			z = (y[i,5] - std_[5])//mean_[5] 
+			# embed()
+			vect_temp.append(np.array([r,p,w,x,v,z]))
+
+
+
+
+		return np.array(vect_temp)
+
+
+	def dany_error_std(self,y,nrow):
+		import math
+		pi = math.pi
+		ORI = []
+		TRA = []
+		temp = []
+		NORMA = 0
+
+		y_std = self.standardization(y,nrow)
+		
+		yp_std = self.standardization(self.y_pred,nrow)
+		
+
+
+		for i in range(0,nrow):
+
+			dx = T.min([abs(y_std[i,0]-yp_std[i,0]),(2*pi - abs(y_std[i,0]-yp_std[i,0]))])
+			dy = T.min([abs(y_std[i,1]-yp_std[i,1]),(2*pi - abs(y_std[i,1]-yp_std[i,1]))])
+			dz = T.min([abs(y_std[i,2]-yp_std[i,2]),(2*pi - abs(y_std[i,2]-yp_std[i,2]))])
+			d_o = np.sqrt(np.add( np.add(T.pow(dx, 2),T.pow(dy, 2)),T.pow(dz, 2)))
+			tx = np.array(y_std[i,3]-yp_std[i,3])
+			ty = np.array(y_std[i,4]-yp_std[i,4])
+			tz = np.array(y_std[i,5]-yp_std[i,5])
+			t0 = np.array([tx,ty,tz])
+			# embed()
+
+			# temp = np.sqrt(np.add( np.add(T.pow(y_std[i,3]-yp_std[i,3], 2),T.pow(y_std[i,4]-yp_std[i,4], 2)),T.pow(y_std[i,5]-yp_std[i,5], 2)))
+
+			# temp.append(np.add(d_o*0.031,0.005*np.linalg.norm(t0,2)))
+			# NORMA +=(np.add(d_o*0.031,0.005*np.linalg.norm(t0,2)))
+			NORMA +=(np.add(d_o,np.linalg.norm(t0,2)))
+			# print NORMA
+			# print (np.add(d_o*0.031,0.005*np.linalg.norm(t0,2)))
+			ORI.append(d_o*0.031)
+			TRA.append(0.005*np.linalg.norm(t0,2))
+
+		# embed()
+		return NORMA,y,self.y_pred,T.cast(ORI,'float32'),T.cast(TRA,'float32')
+
+
+
 	def dany_error(self,y,nrow):
 		import math
 		pi = math.pi
@@ -96,7 +186,7 @@ class ContOutputLayer(object):
 			return valid_sum / valid_num
 		else:
 			assert y.ndim == self.y_pred.ndim, 'Dimension mismatch'
-			return T.mean(T.pow(y-self.y_pred, 2))
+			return T.mean(T.pow(y-self.y_pred, 2)),self.y_pred
 
 	def cost2(self, y):
 		diff = y - self.y_pred
